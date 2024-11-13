4eb4c8cc858a973f90cffffada09412052ea96c8
rodolfoescobarrios <programadorescobar@gmail.com>
2024-11-13 05:26:01 -0300

    '*'

--- inmersion/settings.py
+++ inmersion/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure--o%d-5g$i*uruzs2q^)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'core.Usuario'
 
